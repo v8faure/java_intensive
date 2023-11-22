6e21ad6c592bffbd88ff76071cbe57e9be0f3a6a
Valentine <v8faure@gmail.com>
2023-11-22 17:01:29 +0300

    fixed
    по заданию все готово. можно добавить проверку на некорретные значения

--- java_day_00/ex02/Program.java
+++ java_day_00/ex02/Program.java
@@ -1,9 +1,5 @@
-// read from console until 42
-// sum of digit
-// is prime
-// print Count of coffee-request –
-
 import java.util.Scanner;
+
 public class Program {
 	public static int sum_of_digit(int number) {
 		int sum = 0;
@@ -26,6 +22,7 @@ public class Program {
         }
 		return status;
 	}
+
     public static void main(String[] args) {
 		int count = 0;
 		int number;
@@ -36,7 +33,6 @@ public class Program {
 				count++;
 			}
 		}
-
 		System.out.println("Count of coffee-request - " + count);
 	}
 }
