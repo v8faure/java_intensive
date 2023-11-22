e8ef8c8a3e24b33466bb6790dc15fcaa6844b4d3
Valentine <v8faure@gmail.com>
2023-11-22 14:53:45 +0300

    first ex works
    по заданию должно работать только с 6ти значным числом передаваемым через переменную

---
+++ java_day_00/ex00/Program.java
@@ -0,0 +1,22 @@
+
+public class Program {
+    public static void main(String[] args) {
+        int number = 479598;
+        int sum = 0;
+
+        sum = sum + number%10;
+        number = number/10;
+        sum = sum + number%10;
+        number = number/10;
+        sum = sum + number%10;
+        number = number/10;
+        sum = sum + number%10;
+        number = number/10;
+        sum = sum + number%10;
+        number = number/10;
+        sum = sum + number%10;
+        number = number/10;
+
+        System.out.println(sum);
+    }
+}
